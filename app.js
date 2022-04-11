34ca9601d17804d336da2ac03877e234ab08693e
Alan <415400755@qq.com>
2022-04-11 22:57:27 +0800

    connect mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa  = require('koa')
 const Router = require('koa-router')
+const mongoose = require('mongoose')
 
 const app = new Koa()
 const router = new Router()
@@ -7,6 +8,14 @@ const router = new Router()
 router.get('/', async ctx => {
     ctx.body = {msg: 'Hello Koa!'}
 })
+
+mongoose.connect("mongodb://admin:123456@localhost:27017")
+        .then(() => {
+            console.log('Connected...');
+        })
+        .catch(err => {
+            console.log(err);
+        })
 
 app.use(router.routes()).use(router.allowedMethods())
 
